41c0f7b9e6c5843a367720bc7bcf2e670aeb1cd7
nooria <137643989+noriakeivanfard@users.noreply.github.com>
2023-10-12 21:31:34 +0330

    Update and rename change face.py to change face_1.py

---
+++ assign1/change face_1.py
@@ -0,0 +1,12 @@
+import cv2
+import cvzone
+
+image_1 = cv2.imread("img/happy_face.jpg")
+image_1 = cv2.resize(image_1, (300,500))
+image_2 = cv2.imread("img/sad_face.png",cv2.IMREAD_UNCHANGED)
+image_2 = cv2.resize(image_2, (170, 90))
+img = image_1[50:500,50:500]
+image_1[50:500,50:500] = cvzone.overlayPNG(img, image_2)
+cv2.imwrite(r"c:\Users\Shirin\Desktop\change_face_1.jpg", image_1)
+cv2.imshow("sad_face",image_1)
+cv2.waitKey(0) 
