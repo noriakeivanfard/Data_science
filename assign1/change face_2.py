3354db5ef96ddc4c8f89e1d6d38eaa55eb67980a
nooria <137643989+noriakeivanfard@users.noreply.github.com>
2023-10-12 21:27:16 +0330

    Update change face_2.py

--- assign1/change face_2.py
+++ assign1/change face_2.py
@@ -1,1 +1,13 @@
+import cv2
+import cvzone
+
+image_1 = cv2.imread("img/happy_face.jpg")
+image_1 = cv2.resize(image_1, (300,500))
+image_2 = cv2.imread("img/surprise_face.png",cv2.IMREAD_UNCHANGED)
+image_2 = cv2.resize(image_2, (170, 90))
+img = image_1[50:500,50:500]
+image_1[50:500,50:500] = cvzone.overlayPNG(img, image_2)
+cv2.imwrite(r"c:\Users\Shirin\Desktop\change_face_2.jpg", image_1)
+cv2.imshow("surprise_face",image_1)
+cv2.waitKey(0) 
 
